2981b796fd54991478c9157f765a845227014ba1
jemb4 <jesus8.mb@gmail.com>
2022-06-09 08:39:09 +0000

    diagram

--- src/models.py
+++ src/models.py
@@ -8,26 +8,54 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'User'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    username = Column(String(16), nullable=False)
+    password = Column(String(30), nullable=False)
+    firstname = Column(String(30))
+    lastname = Column(String(30))
+    email = Column(String(120), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Post(Base):
+    __tablename__ = "Post"
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
 
-    def to_dict(self):
-        return {}
+class Vehicles(Base):
+    __tablename__ = "Vehicles"
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    url = Column(String)
+    post_id = Column(Integer, ForeignKey('Post.id'))
+
+class Character(Base):
+    __tablename__ = "Character"
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    birth_year = Column(Integer)
+    eye_color = Column(String)
+    url = Column(String)
+    post_id = Column(Integer, ForeignKey('Post.id'))
+
+class Planet(Base):
+    __tablename__ = "Planet"
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    population = Column(Integer)
+    terrain = Column(String)
+    url = Column(String)
+    post_id = Column(Integer, ForeignKey('Post.id'))
+    
+
+class Favorites(Base):
+    __tablename__ = "Fav"
+    id = Column(Integer, primary_key=True)
+    post_id = Column(Integer, ForeignKey('Post.id'))
+    user_id = Column(Integer, ForeignKey('User.id'))
+
+
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
